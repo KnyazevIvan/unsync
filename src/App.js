0ef3e57b2585079e87f1b3e7c04a7d4bb2c3e1b3
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-03-13 14:36:56 +0300

    first commssadist

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Nav from "./navbar";
 import Todo from "./Todo";
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, HashRouter } from 'react-router-dom';
 import Calendar from './Calendar/Calendar'
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <div>
       
        <Nav/>
        <Route path='/Calendar' render={() => <Calendar />} />
       <Route path='/todo' render={() => <Todo />} />
     </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
